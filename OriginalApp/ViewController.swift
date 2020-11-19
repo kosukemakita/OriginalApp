b0241de7a073366d80c557e7caf86100a7203ed7
kosukemakita <makitakosuke0114@gmail.com>
2020-11-19 21:56:26 +0900

    Initial Commit

---
+++ OriginalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OriginalApp
+//
+//  Created by 蒔田幸祐 on 2020/11/19.
+//  Copyright © 2020 kousuke.makita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
